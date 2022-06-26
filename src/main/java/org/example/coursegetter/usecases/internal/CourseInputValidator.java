e40b3dd076c1d5e0826e9b8ed150933cc660a240
ICPRplshelp <93059453+ICPRplshelp@users.noreply.github.com>
2022-06-26 13:01:45 -0400

    Created CourseInputValidator

---
+++ src/main/java/org/example/coursegetter/usecases/internal/CourseInputValidator.java
@@ -0,0 +1,57 @@
+package org.example.coursegetter.usecases.internal;
+
+import org.example.requisitechecker.usecases.internal.CourseRegexSearcher;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CourseInputValidator {
+
+    private final Pattern[] coursePatterns = {Pattern.compile("[A-Z]{3}[0-4]\\d{2}[HY][01]-[FSY]"),
+            Pattern.compile("[A-Z]{3}[0-4]\\d{2}[HY][01][FSY]"),
+            Pattern.compile("[A-Z]{3}[0-4]\\d{2}[HY][01] [FSY]")};
+    private final CourseRegexSearcher cRgxSearcher = new CourseRegexSearcher();
+
+    /**
+     * Looks for the first course in the string and returns it.
+     *
+     * @param input the string to check.
+     * @return the first regex match of a course, or null otherwise.
+     **/
+    String courseToSearchableCourse(String input) {
+        return cRgxSearcher.lookForCourse(input);
+    }
+
+    /**
+     * Given a course offering, return what needs to be
+     * searched in UofT's timetable API.
+     *
+     * @param input a course entry in the form CSC110Y1-F.
+     * @return the course entry that can be searched in the
+     * UofT timetable API - CSC110Y1-F-20229.
+     * Null if the course is in an invalid format.
+     */
+    String courseOfferingToSearchableCourse(String input) {
+        for (int i = 0; i < this.coursePatterns.length; i++) {
+            Matcher matcher = coursePatterns[i].matcher(input);
+            if (!matcher.find()) {
+                continue;
+            }
+            String matched = matcher.group(0);
+            switch (i) {
+                case 0:  // the standard
+                    return matched;
+                case 1:
+                    return matched.substring(0, matched.length() - 1) + "-"
+                            + matched.charAt(matched.length() - 1);
+                case 2:
+                    String[] splitString = matched.split(" ");
+                    return splitString[0] + "-" + splitString[1];
+                default:
+                    return null;
+            }
+        }
+        return null;
+    }
+
+}
--- src/main/java/org/example/coursegetter/usecases/internal/CourseSearcherIndividual.java
+++ src/main/java/org/example/coursegetter/usecases/internal/CourseSearcherIndividual.java
@@ -6,8 +6,6 @@ import org.example.coursegetter.entities.CourseStorage;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * This class may be used to:
@@ -19,10 +17,8 @@ import java.util.regex.Pattern;
  */
 public class CourseSearcherIndividual {
 
-    private CourseStorage courseStorage;
-    private final Pattern[] coursePatterns = {Pattern.compile("[A-Z]{3}[0-4]\\d{2}[HY][01]-[FSY]"),
-            Pattern.compile("[A-Z]{3}[0-4]\\d{2}[HY][01][FSY]"),
-            Pattern.compile("[A-Z]{3}[0-4]\\d{2}[HY][01] [FSY]")};
+    private final CourseStorage courseStorage;
+    private final CourseInputValidator courseInputValidator = new CourseInputValidator();
 
     public CourseSearcherIndividual(CourseStorage courseStorage) {
         this.courseStorage = courseStorage;
@@ -35,15 +31,29 @@ public class CourseSearcherIndividual {
      * @return a collection of courses such as MAT135H1-F, MAT135H1-S, MAT135H1-Y
      */
     public Collection<Course> getCourseByCourseCode(String crsCode){
+        crsCode = courseInputValidator.courseToSearchableCourse(crsCode);
         ArrayList<Course> courseList = new ArrayList<>();
         String[] suffixes = {"-F", "-S", "-Y"};
         for(String suffix : suffixes){
-            var courseToSearch = crsCode + suffix;
-            var tempCourse = getCourseOfferingByCode(courseToSearch);
+            String courseToSearch = crsCode + suffix;
+            Course tempCourse = getCourseOfferingByCode(courseToSearch);
             if(tempCourse != null)
                 courseList.add(tempCourse);
         }
         return courseList;
+    }
+
+    /**
+     * Gets a course.
+     *
+     * @param crsCode the course code, in a format similar to CSC110Y1-F
+     * @return the Course if one is found, or null otherwise.
+     */
+    public Course getCourseOfferingByCode(String crsCode) {
+        String searchableCourse = courseInputValidator.courseOfferingToSearchableCourse(crsCode);
+        if(searchableCourse == null) return null;
+        searchableCourse += "-" + courseStorage.session;
+        return courseStorage.getCourse(searchableCourse);
     }
 
     /**
@@ -64,51 +74,6 @@ public class CourseSearcherIndividual {
      */
     public Set<String> getAllCoursesList(){
         return courseStorage.getCourseListAsString();
-    }
-
-
-    /**
-     * Gets a course.
-     *
-     * @param crsCode the course code, in a format similar to CSC110Y1-F
-     * @return the Course if one is found, or null otherwise.
-     */
-    public Course getCourseOfferingByCode(String crsCode) {
-        String searchableCourse = courseToSearchableCourse(crsCode);
-        if(searchableCourse == null) return null;
-        return courseStorage.getCourse(searchableCourse);
-    }
-
-    /**
-     * Given a course offering, return what needs to be
-     * searched in UofT's timetable API.
-     *
-     * @param input a course entry in the form CSC110Y1-F.
-     * @return the course entry that can be searched in the
-     *         UofT timetable API - CSC110Y1-F-20229.
-     *         Null if the course is in an invalid format.
-     */
-    private String courseToSearchableCourse(String input) {
-        for (int i = 0; i < this.coursePatterns.length; i++) {
-            Matcher matcher = coursePatterns[i].matcher(input);
-            if (!matcher.find()) {
-                continue;
-            }
-            String matched = matcher.group(0);
-            switch (i) {
-                case 0:  // the standard
-                    return matched + "-" + courseStorage.session;
-                case 1:
-                    return matched.substring(0, matched.length() - 1) + "-"
-                            + matched.charAt(matched.length() - 1) + "-" + courseStorage.session;
-                case 2:
-                    String[] splitString = matched.split(" ");
-                    return splitString[0] + "-" + splitString[1] + "-" + courseStorage.session;
-                default:
-                    return null;
-            }
-        }
-        return null;
     }
 
 }
