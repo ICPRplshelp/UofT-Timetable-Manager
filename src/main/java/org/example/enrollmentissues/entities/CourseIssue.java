fe94a0ecdb135d10787733d1d15e5132418b2bf6
ICPRplshelp <93059453+ICPRplshelp@users.noreply.github.com>
2022-07-16 00:33:20 -0400

    Added classes that represent course issues

---
+++ src/main/java/org/example/enrollmentissues/entities/CourseIssue.java
@@ -0,0 +1,14 @@
+package org.example.enrollmentissues.entities;
+
+
+/**
+ * A class representing an issue with a course.
+ */
+public abstract class CourseIssue {
+    // TODO: This class will likely be empty, but that is okay.
+    // Classes that should extend this:
+    // FYF issue
+    // Prereq issue
+    // Coreq issue
+    // Excl issue
+}
---
+++ src/main/java/org/example/enrollmentissues/entities/Problem.java
@@ -0,0 +1,11 @@
+package org.example.enrollmentissues.entities;
+
+public enum Problem {
+    CONFLICT,
+    TO_FAR,
+    FROM_FAR,
+    NO_PREREQUISITE,
+    NO_COREQUISITE,
+    HAS_EXCLUSION,
+    FYF
+}
