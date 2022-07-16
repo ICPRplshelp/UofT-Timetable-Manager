9f35b90d2eac52dd71bdd8b6b0d8b6bfa4175930
Alex <alexhemo2013@gmail.com>
2022-07-15 18:09:29 -0700

    Added some StudentManager methods

--- src/main/java/org/example/studentdata/usecases/StudentManager.java
+++ src/main/java/org/example/studentdata/usecases/StudentManager.java
@@ -1,7 +1,13 @@
 package org.example.studentdata.usecases;
 
 import org.example.coursegetter.entities.Course;
+import org.example.studentdata.entities.CourseChoice;
 import org.example.studentdata.entities.Student;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StudentManager {
 
@@ -12,11 +18,26 @@ public class StudentManager {
     }
 
     // TODO: implement (alex + hanmin?)
-    public boolean addCourse(Course course) {
+    // I'm gonna make this better soon
+    public boolean addPlannedCourse(List<CourseChoice> courseList) {
+        student.addToPlannedCourses(courseList);
         return true;
     }
 
-    public boolean removeCourse(Course course) {
+    public boolean removePlannedCourse(List<CourseChoice> courseList) {
+        student.removeFromPlannedCourses(courseList);
+        return true;
+    }
+
+    public boolean addPreviousCourse(Collection<CourseChoice> courseList) {
+        student.addToPreviousCourses(courseList.stream()
+                .map(CourseChoice::getCourse).toList());
+        return true;
+    }
+
+    public boolean removePreviousCourse(Collection<CourseChoice> courseList) {
+        student.removeFromPreviousCourses(courseList.stream()
+                .map(CourseChoice::getCourse).toList());
         return true;
     }
 
@@ -24,6 +45,27 @@ public class StudentManager {
         return true;
     }
 
+    public String getPlannedCourses(String session) {
+        Collection<CourseChoice> returnCourses;
+        switch (session) {
+            case "F" -> returnCourses = student.getPlannedFCourses();
+            case "S" -> returnCourses = student.getPlannedSCourses();
+            case "Y" -> returnCourses = student.getPlannedYCourses();
+            default -> {
+                return "Error"; }
+        }
+
+        List<String> coursesList = toCourseNameHelper(returnCourses);
+        return String.join(", ", coursesList);
+
+    }
+
+    private List<String> toCourseNameHelper(Collection<CourseChoice> coursesList) {
+        return coursesList.stream()
+                .map(CourseChoice::getCourse).toList().stream()
+                .map(Course::getCourseTitle)
+                .collect(Collectors.toList());
+    }
     public String getCourseHistory() {
         return "placeholder";
     }
