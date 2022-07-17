ebd1cf48c0c1c9d1045f32d7c714cc0feb3d4211
Caspian-9 <lakeharmony6854@gmail.com>
2022-07-16 22:37:23 -0700

    implemented promptSearchCourse and corresponding methods in Presenter

--- src/main/java/org/example/logincode/interfaceadapters/controllerinput/ControllerInputCourseSearch.java
+++ src/main/java/org/example/logincode/interfaceadapters/controllerinput/ControllerInputCourseSearch.java
@@ -1,11 +1,14 @@
 package org.example.logincode.interfaceadapters.controllerinput;
 
 import org.example.coursegetter.usecases.CourseCommunicator;
+import org.example.coursegetter.usecases.CourseSearcherCommunicator;
 import org.example.coursegetter.usecases.CourseSearcherGetter;
 import org.example.coursegetter.usecases.CourseSearcherIndividual;
 import org.example.logincode.interfaceadapters.Presenter;
 import org.example.logincode.usecases.AccountManager;
 import org.example.logincode.usecases.StorageManager;
+
+import java.util.Collection;
 
 public class ControllerInputCourseSearch extends ControllerInput {
 
@@ -27,17 +30,48 @@ public class ControllerInputCourseSearch extends ControllerInput {
 
     @Override
     public boolean inputParser(String input) {
-        return false;
+        switch (input) {
+            case "record" -> recordPastCourse();
+            case "search" -> promptSearchCourse();
+            case "add" -> addCourseToTimetable();
+            default -> {
+                return failedAction();
+            }
+        }
+        return true;
+    }
+
+    private void recordPastCourse() {
+
+//        String course = "CSC110Y1";
     }
 
     // I need all of its lecture sections (no need for timings)
     private void promptSearchCourse(){
-        String searchedCourse = "CSC110Y1-F";
-        String session = "20229";
+
+        // placeholder
+        String searchedCourse = presenter.enterCourse();
+        String session = presenter.enterSession();
+
         // use CourseSearcherCommunicator to extract searched courses without
         // the need to violate clean architecture.
+        CourseSearcherCommunicator csc = new CourseSearcherCommunicator(courseSearcher);
+        CourseCommunicator courseCommunicator = csc.searchCourse(session, searchedCourse);
 
+        if (courseCommunicator == null) {
+            presenter.genericFailedAction("invalid");
+        } else {
+            Collection<String> lectures = courseCommunicator.getLectures();
+            Collection<String> tutorials = courseCommunicator.getTutorials();
+            Collection<String> practicals = courseCommunicator.getPracticals();
 
+            presenter.printCourseSessionsByType("LEC", lectures);
+            presenter.printCourseSessionsByType("TUT", tutorials);
+            presenter.printCourseSessionsByType("PRA", practicals);
+        }
+    }
+
+    private void addCourseToTimetable() {
 
     }
 
