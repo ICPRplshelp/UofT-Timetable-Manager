37b576b46c732516541b8a940badb90b02a7ed55
Hanmin Kim <hanminkim123@gmail.com>
2022-07-22 01:13:38 -0700

    remove useless variables in EnrollmentControl

--- src/main/java/org/example/coursegetter/entities/EnrollmentControl.java
+++ src/main/java/org/example/coursegetter/entities/EnrollmentControl.java
@@ -5,61 +5,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-// TODO: Choose which variables to keep, and make getters for them
 public class EnrollmentControl implements Serializable {
-    private final String postId;
     private final String postCode;
     private final String postName;
-    private final String subjectId;
-    private final String subjectCode;
     private final String subjectName;
-    private final String designationId;
-    private final String designationCode;
     private final String designationName;
     private final String yearOfStudy;
-    private final String typeOfProgramId;
-    private final String typeOfProgramCode;
     private final String typeOfProgramName;
-    private final String primaryOrgId;
-    private final String primaryOrgCode;
     private final String primaryOrgName;
-    private final String secondaryOrgId;
-    private final String secondaryOrgCode;
     private final String secondaryOrgName;
-    private final String assocOrgId;
-    private final String assocOrgCode;
     private final String assocOrgName;
-    private final String adminOrgId;
-    private final String adminOrgCode;
     private final String adminOrgName;
     private final String restrictedGroup;
 
 
     public EnrollmentControl(Map<String, String> controls) {
-        this.postId = controls.get("postId");
         this.postCode = controls.get("postCode");
         this.postName = controls.get("postName");
-        this.subjectId = controls.get("subjectId");
-        this.subjectCode = controls.get("subjectCode");
         this.subjectName = controls.get("subjectName");
-        this.designationId = controls.get("designationId");
-        this.designationCode = controls.get("designationCode");
         this.designationName = controls.get("designationName");
         this.yearOfStudy = controls.get("yearOfStudy");
-        this.typeOfProgramId = controls.get("typeOfProgramId");
-        this.typeOfProgramCode = controls.get("typeOfProgramCode");
         this.typeOfProgramName = controls.get("typeOfProgramName");
-        this.primaryOrgId = controls.get("primaryOrgId");
-        this.primaryOrgCode = controls.get("primaryOrgCode");
         this.primaryOrgName = controls.get("primaryOrgName");
-        this.secondaryOrgId = controls.get("secondaryOrgId");
-        this.secondaryOrgCode = controls.get("secondaryOrgCode");
         this.secondaryOrgName = controls.get("secondaryOrgName");
-        this.assocOrgId = controls.get("assocOrgId");
-        this.assocOrgCode = controls.get("assocOrgCode");
         this.assocOrgName = controls.get("assocOrgName");
-        this.adminOrgId = controls.get("adminOrgId");
-        this.adminOrgCode = controls.get("adminOrgCode");
         this.adminOrgName = controls.get("adminOrgName");
         this.restrictedGroup = controls.get("restrictedGroup");
     }
