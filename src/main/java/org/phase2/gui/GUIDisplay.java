20f8d069e5c49423fd60d37fecd89ca695320858
Hanmin Kim <hanminkim123@gmail.com>
2022-08-02 17:43:52 -0700

    add barebones GUI components

---
+++ src/main/java/org/phase2/gui/GUIDisplay.java
@@ -0,0 +1,17 @@
+package org.phase2.gui;
+
+import javax.swing.*;
+
+public class GUIDisplay {
+    private JPanel mainPanel;
+    private JPanel buttonsPanel;
+    private JPanel parentPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JPanel admin;
+    private JPanel search;
+    private JPanel standard;
+    private JPanel student;
+}
