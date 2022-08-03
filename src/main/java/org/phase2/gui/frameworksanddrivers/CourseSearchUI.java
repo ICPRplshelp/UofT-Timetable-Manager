d3dad46274c06832629799499bba31aa2ef34923
Hanmin Kim <hanminkim123@gmail.com>
2022-08-03 14:27:13 -0700

    connect search controller to search GUI

--- src/main/java/org/phase2/gui/frameworksanddrivers/CourseSearchUI.java
+++ src/main/java/org/phase2/gui/frameworksanddrivers/CourseSearchUI.java
@@ -3,6 +3,9 @@ package org.phase2.gui.frameworksanddrivers;
 import org.example.logincode.controllerspresentersgateways.controllers.ControllerCourseSearcher2;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Set;
 
 public class CourseSearchUI extends JPanel {
     public JPanel getMainPanel() {
@@ -12,8 +15,9 @@ public class CourseSearchUI extends JPanel {
     private JPanel mainPanel;
     private JButton searchCoursesButton;
     private JButton searchMeetingsButton;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField meetingSearchField;
+    private JTextField courseSearchField;
+    private JTextArea outputTextArea;
 
     private JButton courseButton;
 
@@ -24,6 +28,14 @@ public class CourseSearchUI extends JPanel {
         this.ccs2 = ccs2;
 
         mainPanel.setVisible(true);
+        searchCoursesButton.addActionListener(e -> {
+            Set<String> cc = ccs2.searchCurrentCourses(courseSearchField.getText());
+            outputTextArea.setText(String.join("\n", cc));
+        });
+        searchMeetingsButton.addActionListener(e -> {
+            Set<String> cc = ccs2.searchMeetings(meetingSearchField.getText());
+            outputTextArea.setText(String.join("\n", cc));
+        });
     }
 
 
