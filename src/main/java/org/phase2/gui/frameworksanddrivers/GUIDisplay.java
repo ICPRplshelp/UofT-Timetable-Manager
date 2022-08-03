be8b4e390d9af0d2dbe7a5bf01c1475e4e8d4cb7
Hanmin Kim <hanminkim123@gmail.com>
2022-08-03 15:16:37 -0700

    use lambda

--- src/main/java/org/phase2/gui/frameworksanddrivers/GUIDisplay.java
+++ src/main/java/org/phase2/gui/frameworksanddrivers/GUIDisplay.java
@@ -27,48 +27,16 @@ public class GUIDisplay {
 
     private final LoginUI loginUI;
 
-
-    private final PresenterPrinter prt = new PresenterPrinter();
-
     public GUIDisplay() {
         loginUI = new LoginUI(this);
         switchLoginView("logout");
 
-
-        // these are temporary buttons to test the GUI, they will be removed later
-        studentButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                switchViews("student");
-            }
-        });
-        standardButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                switchViews("standard");
-            }
-        });
-        searchButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                switchViews("search");
-            }
-        });
-        adminButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                switchViews("admin");
-            }
-        });
-        logoutButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                switchLoginView("logout");
-            }
-        });
+        studentButton.addActionListener(e -> switchViews("student"));
+        standardButton.addActionListener(e -> switchViews("standard"));
+        searchButton.addActionListener(e -> switchViews("search"));
+        adminButton.addActionListener(e -> switchViews("admin"));
+        logoutButton.addActionListener(e -> switchLoginView("logout"));
     }
-
-    // there are the methods that will be kept
 
     public void switchViews(String view){
         bodyPanel.removeAll();
