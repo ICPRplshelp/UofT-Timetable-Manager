2862f1dce76a0f4892e4942dccae21525d0fd726
Hanmin Kim <hanminkim123@gmail.com>
2022-08-10 16:16:59 -0700

    update AccountTests to use assertTrue and assertNull use non-deprecated Date constructor method

--- src/test/java/p2tests/AccountTests.java
+++ src/test/java/p2tests/AccountTests.java
@@ -2,13 +2,12 @@ package p2tests;
 
 import org.example.logincode.controllerspresentersgateways.gateways.StorageLoader;
 import org.example.logincode.entities.Account;
-import org.example.logincode.entities.AccountStorage;
 import org.example.logincode.usecases.*;
 import org.junit.Test;
 
 import java.util.Date;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AccountTests {
 
@@ -19,7 +18,7 @@ public class AccountTests {
         assertEquals("password", StorageManager.getInstance(new StorageLoader()).getAccount("User").getPassword());
         AccountLogin accountLogin = new AccountLogin(StorageManager.getInstance(new StorageLoader()));
         assertEquals("User", accountLogin.login("User", "password").getUsername());
-        assertEquals(true, accountLogin.validateLogin("User", "password"));
+        assertTrue(accountLogin.validateLogin("User", "password"));
     }
 
     @Test(timeout = 1200)
@@ -33,11 +32,11 @@ public class AccountTests {
         accountManager.makeMeAnAdmin();
         AdminAccountManager adminAccountManager = new AdminAccountManager(account, StorageManager.getInstance(new StorageLoader()));
         adminAccountManager.createNewAdminUser("test1", "test2");
-        assertEquals(true, StorageManager.getInstance(new StorageLoader()).getAccount("test1").getPermissions().hasPerm("admin"));
-        adminAccountManager.banUser("Placeholder", new Date(2022, 12, 31));
-        assertEquals(true, StorageManager.getInstance(new StorageLoader()).getAccount("Placeholder").getBanStatus().isBanned());
+        assertTrue(StorageManager.getInstance(new StorageLoader()).getAccount("test1").getPermissions().hasPerm("admin"));
+        adminAccountManager.banUser("Placeholder", new Date(1672473600000L));
+        assertTrue(StorageManager.getInstance(new StorageLoader()).getAccount("Placeholder").getBanStatus().isBanned());
         adminAccountManager.deleteUser("Placeholder");
-        assertEquals(null, StorageManager.getInstance(new StorageLoader()).getAccount("Placeholder"));
+        assertNull(StorageManager.getInstance(new StorageLoader()).getAccount("Placeholder"));
     }
 
 
