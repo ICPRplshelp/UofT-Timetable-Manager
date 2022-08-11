f155e4e230469013b000b340fa4f19bab5a8c82c
ICPRplshelp <93059453+ICPRplshelp@users.noreply.github.com>
2022-08-10 23:00:27 -0400

    Added RunGUIDisplay for the purpose of reducing the need to navigate folders to get there

---
+++ src/main/java/org/RunGUIDisplay.java
@@ -0,0 +1,15 @@
+package org;
+
+import org.example.gui.GUIDisplay;
+
+/**
+ * Use this class to boot up the GUI display.
+ * In case marking two main methods for this project is
+ * not possible, mark this by default.
+ */
+public class RunGUIDisplay {
+    public static void main(String[] args) {
+        String[] arguments = {};
+        GUIDisplay.main(arguments);
+    }
+}
